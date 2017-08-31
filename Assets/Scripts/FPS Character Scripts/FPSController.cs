2f9381c63e0a031dfad24e136aa1087a3ee14538
Riz1 <rizowan69@gmail.com>
2017-08-31 14:02:17 +0600

    First Upload

---
+++ Assets/Scripts/FPS Character Scripts/FPSController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour {
+
+    private Transform firsPerson_View;
+    private Transform firstPerson_Camera;
+
+    private Vector3 firstPerson_View_Rotation = Vector3.zero;
+
+    public float walkSpeed = 6.75f;
+    public float runSpeed = 10.0f;
+    public float crouchSpeed = 4.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20f;
+
+    private float speed;
+
+    private bool is_moving, is_Grounded, is_Crouching;
+
+    private float inputX, inputY;
+    private float inputX_Set, inputY_Set;
+    private float inputModifyFactor;
+
+    private bool limitDiagonalSpeed = true;
+
+    private float antiBumpFactor = 0.75f;
+
+    private CharacterController charController;
+    private Vector3 moveDirection = Vector3.zero;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
