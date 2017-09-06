61ad6652822d23ca07f396e9437b8f33f395f46e
Riz1 <rizowan69@gmail.com>
2017-09-06 12:25:27 +0600

    animation

--- Assets/Scripts/FPS Character Scripts/FPSController.cs
+++ Assets/Scripts/FPS Character Scripts/FPSController.cs
@@ -9,6 +9,14 @@ public class FPSController : MonoBehaviour {
 
     private Vector3 firstPerson_View_Rotation = Vector3.zero;
 
+    public LayerMask groundLayer;
+    private float rayDistance;
+    private float default_ControllerHeight;
+    private Vector3 default_CamPos;
+    private float camHeight; 
+
+    private FPSPlayerAnimation playerAnimation;
+
     public float walkSpeed = 6.75f;
     public float runSpeed = 10.0f;
     public float crouchSpeed = 4.0f;
@@ -36,6 +44,12 @@ public class FPSController : MonoBehaviour {
         charController = GetComponent<CharacterController>();
         speed = walkSpeed;
         is_moving = false;
+
+        rayDistance = charController.height * 0.5f + charController.radius;
+        default_ControllerHeight = charController.height;
+        default_CamPos = firsPerson_View.localPosition;
+
+        playerAnimation = GetComponent<FPSPlayerAnimation>();
 	}
 	
 	// Update is called once per frame
@@ -45,7 +59,7 @@ public class FPSController : MonoBehaviour {
 
     void PlayerMovement()
     {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)){
+        if(Input.GetKey(KeyCode.W)  || Input.GetKey(KeyCode.S)){
             if (Input.GetKey(KeyCode.W))
             {
                 inputY_Set = 1f;
@@ -86,13 +100,131 @@ public class FPSController : MonoBehaviour {
 
         if (is_Grounded)
         {
+            PlayerCrouchingAndSprinting();
             moveDirection = new Vector3(inputX * inputModifyFactor, - antiBumpFactor, inputY * inputModifyFactor);
             moveDirection = transform.TransformDirection(moveDirection) * speed;
+
+            PlayerJump();
         }
 
         moveDirection.y -= gravity * Time.deltaTime;
 
         is_Grounded = (charController.Move(moveDirection * Time.deltaTime) & CollisionFlags.Below) != 0;
         is_moving = charController.velocity.magnitude > 0.15f;
+
+        //Animation...................
+        HandleAnimations();
+
     }
+
+    void PlayerCrouchingAndSprinting()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (!is_Crouching)
+            {
+                is_Crouching = true;
+            }
+            else
+            {
+                if (CanGetUp())
+                {
+                    is_Crouching = false;
+                }
+            }
+
+            StopCoroutine(MoveCameraCrouch());
+            StartCoroutine(MoveCameraCrouch());
+
+        }
+        if (is_Crouching)
+        {
+            speed = crouchSpeed;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = runSpeed;
+            }
+            else
+            {
+                speed = walkSpeed;
+            }
+        }
+
+        playerAnimation.PlayerCrouch(is_Crouching);
+    }
+    bool CanGetUp()
+    {
+        Ray groundRay = new Ray(transform.position, transform.up);
+        RaycastHit groundHit;
+        if (Physics.SphereCast(groundRay,charController.radius + .05f, out groundHit, rayDistance, groundLayer))
+        {
+            if(Vector3.Distance(transform.position,groundHit.point) < 2.3f)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    IEnumerator MoveCameraCrouch()
+    {
+        charController.height = is_Crouching ? default_ControllerHeight / 1.5f : default_ControllerHeight;
+        charController.center = new Vector3(0f, charController.height / 2f, 0f);
+
+        camHeight = is_Crouching ? default_CamPos.y / 1.5f : default_CamPos.y;
+
+        while(Mathf.Abs(camHeight - firsPerson_View.localPosition.y) > 0.01f)
+        {
+            firsPerson_View.localPosition = Vector3.Lerp(firsPerson_View.localPosition, new Vector3(default_CamPos.x, camHeight, default_CamPos.z), Time.deltaTime * 11f);
+
+            yield return null;
+        }
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (is_Crouching)
+            {
+                if (CanGetUp())
+                {
+                    is_Crouching = false;
+                    playerAnimation.PlayerCrouch(is_Crouching);
+
+                    StopCoroutine(MoveCameraCrouch());
+                    StartCoroutine(MoveCameraCrouch());
+                }
+            }
+            else
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+    }
+
+
+    //################################# ANIMATION FUNCTION ##########################
+    void HandleAnimations()
+    {
+        playerAnimation.Movement(charController.velocity.magnitude);
+        playerAnimation.PlayerJump(charController.velocity.y);
+
+        if(is_Crouching && charController.velocity.magnitude > 0f)
+        {
+            playerAnimation.PlayerCrouchWalk(charController.velocity.magnitude);
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
